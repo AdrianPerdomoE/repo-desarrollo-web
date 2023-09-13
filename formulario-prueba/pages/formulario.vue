9c427ce7d95c5aec3ea5fd5bc5e8d14bd0d020e1
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-09-12 19:29:10 -0500

    Clonacion del respositorio del profesor e inicio del proyecto de marvel

---
+++ formulario-prueba/pages/formulario.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        hola
+    </div>
+</template>
+<script>
+definePageMeta({
+    layout: "blank",
+});
+</script>
---
+++ frontend_marvel/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLink to="/marvel_caracters">
+    Click para ver personajes
+  </NuxtLink>
+</template>
+<script>
+definePageMeta({
+  layout: "default",
+});
+</script>
