0ad49e544bea779dfc8830b66006d45bdbdf0392
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-09-14 13:35:45 -0500

    feature: Se aumento el limite de personajes y se filtra la lista para mostrar aquellos que tengan una imangen disponible feature: El dialogo muestra los datos correspondientes al resumen de cada personaje, se verifica que si no existe descripcion no se muestre fix: los estilos de las etiquetas se llevaron a clases que se manejan desde un solo style tag

--- frontend_marvel/components/personaje.vue
+++ frontend_marvel/components/personaje.vue
@@ -1,15 +1,57 @@
-<template>
-    <v-card>
-        <v-card width="300" height="300" :title="caracter.name"
-            style="display: flex;align-items: center; justify-content: center; flex-direction: column;">
-            <v-img width="80%" aspect-ratio="3/4" style="border-radius: 5px;margin-bottom: 1px;" cover
-                :src="caracter.thumbnail.path + '.' + caracter.thumbnail.extension"></v-img>
+<template >
+    <v-container style="width: 100dvw; height: 100dvh; background-color: aqua;">
+        <v-row justify="start">
+            <v-col cols="4">
+                <v-img width="80%" aspect-ratio="3/4" class="img" cover
+                    :src="caracter.thumbnail.path + '.' + caracter.thumbnail.extension"></v-img>
+            </v-col>
+            <v-col cols="6" style="text-align: center;">
+                <h1>{{ caracter.name }}</h1>
+                <v-row justify="center">
+                    <v-col :v-if="caracter.description">
+                        <p class="text-center">
+                            {{ caracter.description }}
+                        </p>
+                    </v-col>
+                    <v-col>
+                        <v-table>
+                            <tbody>
+                                <tr>
+                                    <td>Cantidad de comics</td>
+                                    <td>{{ caracter.comics.available }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Cantidad de series</td>
+                                    <td>{{ caracter.series.available }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Cantidad de historias</td>
+                                    <td>{{ caracter.stories.available }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Cantidad de eventos</td>
+                                    <td>{{ caracter.events.available }}</td>
+                                </tr>
+                            </tbody>
+                        </v-table>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="2">
+                <h1>Series</h1>
+            </v-col>
+        </v-row>
+        <v-row justify="center" v-for=" (serie, index) in caracter.series.items" :key="index">
+            <v-col cols="5" v-if="index < 3">
+                <h2>{{ serie.name }}</h2>
+            </v-col>
+        </v-row>
 
-        </v-card>
-    </v-card>
+    </v-container>
 </template>
 <script>
-
 export default {
     props: {
         caracter: Object // Define una propiedad llamada "object" que recibe un objeto como valor
--- frontend_marvel/pages/personajes.vue
+++ frontend_marvel/pages/personajes.vue
@@ -6,15 +6,13 @@
         <v-container>
             <v-row>
                 <v-col v-for="item in caracters" :key="item.id">
-                    <v-card width="300" height="300" @click="openDialog(item)" :title="item.name"
-                        style="display: flex;align-items: center; justify-content: center; flex-direction: column;">
-                        <v-img width="80%" aspect-ratio="3/4" style="border-radius: 5px;margin-bottom: 1px;" cover
-                            :src="item.thumbnail.path + '.' + item.thumbnail.extension"></v-img>
+                    <v-card width="350" height="500" @click="openDialog(item)" :title="item.name" class="card">
+                        <v-img width="340" class="img" :src="item.thumbnail.path + '.' + item.thumbnail.extension"
+                            cover></v-img>
                     </v-card>
-
                     <v-dialog v-model="dialog" width="auto">
                         <personaje :caracter="actualCaracter"></personaje>
-                        <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+                        <v-btn color="primary" block @click="dialog = false">Cerrar</v-btn>
                     </v-dialog>
 
                 </v-col>
@@ -32,6 +30,7 @@ const PRIV_KEY = "dc1c9e15c529c9176fa615bb364c6b538eb8b563";
 const PUBLIC_KEY = "1069f78b41731dbb5ca5cba2f9844744";
 const caracters = ref([])
 const actualCaracter = ref(null)
+
 onBeforeMount(() => {
     loadCaracters()
 })
@@ -43,9 +42,26 @@ const dialog = ref(false);
 const loadCaracters = async () => {
     var ts = new Date().getTime();
     var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
-    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&limit=100`
     const { data } = await axios.get(url)
-    caracters.value = data.data.results
+    caracters.value = data.data.results.filter(caracter => {
+        return !caracter.thumbnail.path.includes("image_not_available");
+    })
     console.log(data.data.results)
 }
-</script>
+</script>`
+<style>
+.card {
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 50px;
+}
+
+.img {
+    border-radius: 100%;
+    margin-bottom: 1px;
+}
+</style>
