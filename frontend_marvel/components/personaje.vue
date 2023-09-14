59b49f9df229f9798b14ff07da99de86f694c98f
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-09-13 22:35:04 -0500

    feature: Implementacion de un modal que ingresa en el prop de la etiqueta de personaje, al personaje actual selecionado para mostrar todos sus detalles

---
+++ frontend_marvel/components/personaje.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card>
+        <v-card width="300" height="300" :title="caracter.name"
+            style="display: flex;align-items: center; justify-content: center; flex-direction: column;">
+            <v-img width="80%" aspect-ratio="3/4" style="border-radius: 5px;margin-bottom: 1px;" cover
+                :src="caracter.thumbnail.path + '.' + caracter.thumbnail.extension"></v-img>
+
+        </v-card>
+    </v-card>
+</template>
+<script>
+
+export default {
+    props: {
+        caracter: Object // Define una propiedad llamada "object" que recibe un objeto como valor
+    }
+}
+</script>
--- frontend_marvel/pages/personajes.vue
+++ frontend_marvel/pages/personajes.vue
@@ -6,12 +6,17 @@
         <v-container>
             <v-row>
                 <v-col v-for="item in caracters" :key="item.id">
-                    <v-card width="300" height="300" @click="redirect(item.id)" :title="item.name"
+                    <v-card width="300" height="300" @click="openDialog(item)" :title="item.name"
                         style="display: flex;align-items: center; justify-content: center; flex-direction: column;">
                         <v-img width="80%" aspect-ratio="3/4" style="border-radius: 5px;margin-bottom: 1px;" cover
                             :src="item.thumbnail.path + '.' + item.thumbnail.extension"></v-img>
-
                     </v-card>
+
+                    <v-dialog v-model="dialog" width="auto">
+                        <personaje :caracter="actualCaracter"></personaje>
+                        <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+                    </v-dialog>
+
                 </v-col>
             </v-row>
         </v-container>
@@ -20,19 +25,21 @@
     </div>
 </template>
 <script setup>
+
 import axios from "axios";
 import CryptoJS from "crypto-js";
 const PRIV_KEY = "dc1c9e15c529c9176fa615bb364c6b538eb8b563";
 const PUBLIC_KEY = "1069f78b41731dbb5ca5cba2f9844744";
 const caracters = ref([])
-
+const actualCaracter = ref(null)
 onBeforeMount(() => {
     loadCaracters()
 })
-
-const redirect = (caracterId) => {
-    console.log(caracterId)
+const openDialog = (caracter) => {
+    actualCaracter.value = caracter;
+    dialog.value = true;
 }
+const dialog = ref(false);
 const loadCaracters = async () => {
     var ts = new Date().getTime();
     var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
